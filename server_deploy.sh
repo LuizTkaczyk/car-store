e141b01d406ee9ab23496af25dfffaa169d9a5c2
Luiz Antonio <luiz8907@hotmail.com>
2024-01-07 16:32:49 -0300

    ci

--- server_deploy.sh
+++ server_deploy.sh
@@ -4,9 +4,6 @@ set -e
 
 echo "Deploying application ..."
 
-# Install dependencies based on lock file
-/opt/cpanel/composer/bin/composer install --no-interaction --prefer-dist --optimize-autoloader
-
 # Migrate database
 php artisan migrate --force
 
